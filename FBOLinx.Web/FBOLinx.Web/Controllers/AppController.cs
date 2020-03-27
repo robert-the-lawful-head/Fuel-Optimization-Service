beb97c5d27867f331465c0f4d31376cdd416eb03
robert-the-lawful-head <robertlawhead1@gmail.com>
2020-03-27 10:26:53 -0700

    Add app version api

---
+++ FBOLinx.Web/FBOLinx.Web/Controllers/AppController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+
+namespace FBOLinx.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AppController : ControllerBase
+    {
+        public AppController()
+        {
+        }
+
+        // GET: api/app/version
+        [HttpGet("version")]
+        public IActionResult GetAppVersion()
+        {
+            string assemblyVersion1 = GetType().Assembly.GetName().Version.ToString();
+            string assemblyVersion2 = Assembly.GetEntryAssembly().GetName().Version.ToString();
+            string fileVersion = Assembly.GetEntryAssembly().GetCustomAttribute<AssemblyFileVersionAttribute>().Version;
+            string version = Assembly.GetEntryAssembly().GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion;
+            List<string> result = new List<string>
+            {
+                assemblyVersion1,
+                assemblyVersion2,
+                fileVersion,
+                version
+            };
+            return Ok(result);
+        }
+    }
+}
