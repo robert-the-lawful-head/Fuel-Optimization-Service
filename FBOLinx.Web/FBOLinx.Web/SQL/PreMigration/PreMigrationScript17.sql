1caf4e91a513dd765ab9bfcdd9f68adf361d3bf9
robert-the-lawful-head <robertlawhead1@gmail.com>
2020-05-07 17:36:46 -0700

    [#68gdxy] Expanded the length of the email column to hold more emails.

--- FBOLinx.Web/FBOLinx.Web/SQL/PreMigration/PreMigrationScript17.sql
+++ FBOLinx.Web/FBOLinx.Web/SQL/PreMigration/PreMigrationScript17.sql
@@ -3,3 +3,7 @@ CREATE NONCLUSTERED INDEX [INX_FBOPrices_EffectiveDates]
 ON [dbo].[FBOPrices] ([EffectiveFrom],[EffectiveTo])
 INCLUDE ([FBOID],[Product],[Price],[Expired])
 GO
+
+ALTER TABLE FuelReq
+	ALTER COLUMN Email varchar(255)
+GO
