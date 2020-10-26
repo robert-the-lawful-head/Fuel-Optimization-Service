3fda849cd218b2376f1fe8eeaafe27f2c8874f27
robert-the-lawful-head <robertlawhead1@gmail.com>
2020-10-26 16:13:35 -0700

    wip

---
+++ FBOLinx.Web/FBOLinx.Web/Models/AFSAircraft.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FBOLinx.Web.Models
+{
+    public class AFSAircraft
+    {
+        [Key]
+        [Column("OID")]
+        public int Oid { get; set; }
+
+        [Column("ICAO")]
+        public string Icao { get; set; }
+
+        public string AircraftTypeName { get; set; }
+
+        public string AircraftTypeEngineName { get; set; }
+
+        public int DegaAircraftID { get; set; }
+    }
+}
