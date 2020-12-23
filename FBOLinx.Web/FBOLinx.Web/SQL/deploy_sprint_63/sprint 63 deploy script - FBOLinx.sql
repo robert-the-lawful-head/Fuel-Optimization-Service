a15c4868525bc0f8fc4fb6c3065a89255af09fee
robert-the-lawful-head <robertlawhead1@gmail.com>
2020-12-23 12:41:26 -0800

    deploy script

---
+++ FBOLinx.Web/FBOLinx.Web/SQL/deploy_sprint_63/sprint 63 deploy script - FBOLinx.sql
@@ -0,0 +1,12 @@
+GO
+update fp
+set EffectiveTo = dateadd(dd, 1, fp.EffectiveTo)
+from fbos f
+inner join fboairports fa on fa.fboid = f.oid
+inner join fboprices fp on fp.fboid = f.oid
+inner join [group] g on g.oid = f.groupid
+where isnull(g.IsFBONetwork, 0) = 0
+and fp.EffectiveTo is not null
+and fp.EffectiveTo > '1/1/2020'
+and fp.Effectiveto < '9999-12-31'
+GO
