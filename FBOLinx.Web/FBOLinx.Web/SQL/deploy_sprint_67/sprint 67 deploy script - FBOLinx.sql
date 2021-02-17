5111532c05dd241f179ef708a39694d345ffa9e7
robert-the-lawful-head <robertlawhead1@gmail.com>
2021-02-17 11:38:36 -0800

    deploy script

---
+++ FBOLinx.Web/FBOLinx.Web/SQL/deploy_sprint_67/sprint 67 deploy script - FBOLinx.sql
@@ -0,0 +1,12 @@
+use [paragon_test]
+GO
+delete fr
+FROM fuelreq fr
+    WHERE isnull(sourceid,0)>0 and datecreated>='2020-12-01 13:03:50.887' and fr.OID NOT IN
+    (
+        SELECT MAX(fr.OID)
+        FROM fuelreq fr
+		where isnull(sourceid,0)>0
+        GROUP BY SourceID
+    )
+GO
