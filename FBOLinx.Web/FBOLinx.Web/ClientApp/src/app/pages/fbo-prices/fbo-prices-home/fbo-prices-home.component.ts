728140119144a5528fc3bf2b63cfeb8239f0050a
robert-the-lawful-head <robertlawhead1@gmail.com>
2021-04-29 12:13:04 -0700

    UI adjustment for staged pricing showing expiration date properly.

--- FBOLinx.Web/FBOLinx.Web/ClientApp/src/app/pages/fbo-prices/fbo-prices-home/fbo-prices-home.component.ts
+++ FBOLinx.Web/FBOLinx.Web/ClientApp/src/app/pages/fbo-prices/fbo-prices-home/fbo-prices-home.component.ts
@@ -545,6 +545,14 @@ export class FboPricesHomeComponent implements OnInit, OnDestroy, AfterViewInit 
                     this.stagedPrices = data;
                     this.stagedFboPriceJetACost = this.getStagedPriceByProduct('JetA Cost');
                     this.stagedFboPriceJetARetail = this.getStagedPriceByProduct('JetA Retail');
+                    if (this.stagedFboPriceJetACost.effectiveTo) {
+                        this.stagedFboPriceJetACost.effectiveTo =
+                            moment(this.stagedFboPriceJetACost.effectiveTo).subtract(1, 'minutes');
+                    }
+                    if (this.stagedFboPriceJetARetail.effectiveTo) {
+                        this.stagedFboPriceJetARetail.effectiveTo =
+                            moment(this.stagedFboPriceJetARetail.effectiveTo).subtract(1, 'minutes');
+                    }
                     this.subscribeToPricingShift();
                     observer.next();
                 }, (error: any) => {
