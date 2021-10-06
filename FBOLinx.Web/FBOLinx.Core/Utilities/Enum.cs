253ffad716e1939d393c5d643226a24eeff1ffa2
robert-the-lawful-head <robertlawhead1@gmail.com>
2021-10-06 12:20:18 +0200

    Complete Backend Customer Analytics part2

--- FBOLinx.Web/FBOLinx.Core/Utilities/Enum.cs
+++ FBOLinx.Web/FBOLinx.Core/Utilities/Enum.cs
@@ -10,10 +10,10 @@ namespace FBOLinx.Core.Utilities
         public static string GetDescription(System.Enum value)
         {
             FieldInfo fi = value.GetType().GetField(value.ToString());
-           // DescriptionAttribute[] attributes = (DescriptionAttribute[])fi.GetCustomAttributes(typeof(DescriptionAttribute), false);
-            //if (attributes.Length > 0)
-              //  return attributes[0].Description;
-            //else
+            DescriptionAttribute[] attributes = (DescriptionAttribute[])fi.GetCustomAttributes(typeof(DescriptionAttribute), false);
+            if (attributes.Length > 0)
+                return attributes[0].Description;
+            else
             return value.ToString();
         }
 
