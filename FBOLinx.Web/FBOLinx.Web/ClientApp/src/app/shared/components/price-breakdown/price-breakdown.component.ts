2c6ba1afd40c8d46e6849a4753b9c28cac19e0e7
robert-the-lawful-head <robertlawhead1@gmail.com>
2021-10-12 02:15:11 +0200

    update tax and fee of ITP Margins

--- FBOLinx.Web/FBOLinx.Web/ClientApp/src/app/shared/components/price-breakdown/price-breakdown.component.ts
+++ FBOLinx.Web/FBOLinx.Web/ClientApp/src/app/shared/components/price-breakdown/price-breakdown.component.ts
@@ -249,6 +249,8 @@ export class PriceBreakdownComponent implements OnInit {
             this.domesticPrivatePricing = responseList[3];
             const customerInfoByGroup = responseList[4];
 
+            console.log(this.domesticPrivatePricing.pricingList[0]);
+
             //If no result was returned for the customer then this wasn't a customer-level price check so mark it as active.
             //Otherwise check the actual active flag of the customer.
             this.isCustomerActive =
