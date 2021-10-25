90c189e88b84210d579c43b081cf91159dfc0ffc
robert-the-lawful-head <robertlawhead1@gmail.com>
2021-10-26 01:54:28 +0200

    Add Audit to Context and Create Auditog Method

--- FBOLinx.Web/FBOLinx.DB/Context/FboLinxContext.cs
+++ FBOLinx.Web/FBOLinx.DB/Context/FboLinxContext.cs
@@ -1,6 +1,9 @@
 ﻿using FBOLinx.DB.Models;
 using FBOLinx.Web;
 using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using static FBOLinx.DB.Models.Audit;
 
 namespace FBOLinx.DB.Context
 {
@@ -13,6 +16,65 @@ namespace FBOLinx.DB.Context
         public FboLinxContext(DbContextOptions<FboLinxContext> options)
             : base(options)
         {
+        }
+
+        public DbSet<Audit> AuditsLogs { get; set; }
+
+
+        public virtual async Task<int> SaveChangesAsync(int userId = 0)
+        {
+            OnBeforeSaveChanges(userId);
+            var result = await base.SaveChangesAsync();
+            return result;
+        }
+
+        //for Add the new / old Data and Serilize it 
+        private void OnBeforeSaveChanges(int userId)
+        {
+            ChangeTracker.DetectChanges();
+            var auditEntries = new List<AuditEntry>();
+            foreach (var entry in ChangeTracker.Entries())
+            {
+                if (entry.Entity is Audit || entry.State == EntityState.Detached || entry.State == EntityState.Unchanged)
+                    continue;
+                var auditEntry = new AuditEntry(entry);
+                auditEntry.TableName = entry.Entity.GetType().Name;
+                auditEntry.UserId = userId;
+                auditEntries.Add(auditEntry);
+                foreach (var property in entry.Properties)
+                {
+                    string propertyName = property.Metadata.Name;
+                    if (property.Metadata.IsPrimaryKey())
+                    {
+                        auditEntry.KeyValues[propertyName] = property.CurrentValue;
+                        continue;
+                    }
+                    switch (entry.State)
+                    {
+                        case EntityState.Added:
+                            auditEntry.AuditType = AuditType.Create;
+                            auditEntry.NewValues[propertyName] = property.CurrentValue;
+                            break;
+                        case EntityState.Deleted:
+                            auditEntry.AuditType = AuditType.Delete;
+                            auditEntry.OldValues[propertyName] = property.OriginalValue;
+                            break;
+                        case EntityState.Modified:
+                            if (property.IsModified)
+                            {
+                                auditEntry.ChangedColumns.Add(propertyName);
+                                auditEntry.AuditType = AuditType.Update;
+                                auditEntry.OldValues[propertyName] = property.OriginalValue;
+                                auditEntry.NewValues[propertyName] = property.CurrentValue;
+                            }
+                            break;
+                    }
+                }
+            }
+            foreach (var auditEntry in auditEntries)
+            {
+                AuditsLogs.Add(auditEntry.ToAudit());
+            }
         }
 
         public virtual DbSet<AccessTokens> AccessTokens { get; set; }
--- FBOLinx.Web/FBOLinx.DB/Models/AuditEntry.cs
+++ FBOLinx.Web/FBOLinx.DB/Models/AuditEntry.cs
@@ -16,7 +16,7 @@ namespace FBOLinx.DB.Models
 
 
         public EntityEntry Entry { get; }
-        public string UserId { get; set; }
+        public int UserId { get; set; }
         public string TableName { get; set; }
         public Dictionary<string, object> KeyValues { get; } = new Dictionary<string, object>();
         public Dictionary<string, object> OldValues { get; } = new Dictionary<string, object>();
