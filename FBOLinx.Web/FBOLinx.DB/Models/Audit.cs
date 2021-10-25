cf62bfb8d29506869879a014e757454a630d6f21
robert-the-lawful-head <robertlawhead1@gmail.com>
2021-10-26 01:49:41 +0200

    Add Audit for Log Part1

---
+++ FBOLinx.Web/FBOLinx.DB/Models/Audit.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FBOLinx.DB.Models
+{
+    public class Audit
+    {
+        public enum AuditType
+        {
+            None = 0,
+            Create = 1,
+            Update = 2,
+            Delete = 3
+        }
+
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public string Type { get; set; }
+        public string TableName { get; set; }
+        public DateTime DateTime { get; set; }
+        public string OldValues { get; set; }
+        public string NewValues { get; set; }
+        public string AffectedColumns { get; set; }
+        public string PrimaryKey { get; set; }
+    }
+}
