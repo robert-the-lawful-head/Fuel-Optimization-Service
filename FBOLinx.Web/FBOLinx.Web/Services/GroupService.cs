d29d23159a29f47a44000cb9cbd169ac5b1ae48e
robert-the-lawful-head <robertlawhead1@gmail.com>
2021-11-05 09:55:16 -0700

    [#1ty5jna] Minor refactoring of the merge groups feature

--- FBOLinx.Web/FBOLinx.Web/Services/GroupService.cs
+++ FBOLinx.Web/FBOLinx.Web/Services/GroupService.cs
@@ -9,6 +9,8 @@ using FBOLinx.DB.Context;
 using Geolocation;
 using FBOLinx.Web.Models.Requests;
 using System.IO;
+using System.Transactions;
+using EFCore.BulkExtensions;
 
 namespace FBOLinx.Web.Services
 {
@@ -72,6 +74,157 @@ namespace FBOLinx.Web.Services
             {
                 _fileStorageContext.FboLinxImageFileData.Remove(existingRecord);
                 await _fileStorageContext.SaveChangesAsync();
+            }
+        }
+
+        public async Task MergeGroups(MergeGroupRequest request)
+        {
+            using (var transaction = _context.Database.BeginTransaction())
+            {
+                try
+                {
+                    var changeableGroups = request.Groups.Where(group => group.Oid != request.BaseGroupId).Select(group => group.Oid).ToList();
+
+                    var users = await _context.User.Where(a => changeableGroups.Contains((a.GroupId ?? 0))).ToListAsync();
+                    var fbos = await _context.Fbos.Where(a => changeableGroups.Contains((a.GroupId ?? 0))).ToListAsync();
+                    var adminEmails = await _context.AdminEmails.Where(a => changeableGroups.Contains(a.GroupId)).ToListAsync();
+                    var companiesByGroups = await _context.CompaniesByGroup.Where(a => changeableGroups.Contains(a.GroupId)).ToListAsync();
+                    var contactInfoByGroups = await _context.ContactInfoByGroup.Where(a => changeableGroups.Contains(a.GroupId)).ToListAsync();
+                    var customerAircrafts = await _context.CustomerAircrafts.Where(a => changeableGroups.Contains((a.GroupId ?? 0))).ToListAsync();
+                    var customerAircraftViewedByGroups = await _context.CustomerAircraftViewedByGroup.Where(a => changeableGroups.Contains((a.GroupId ?? 0))).ToListAsync();
+                    var customerCompanyTypes = await _context.CustomerCompanyTypes.Where(a => changeableGroups.Contains(a.GroupId)).ToListAsync();
+                    var customerInfoByGroups = await _context.CustomerInfoByGroup.Where(a => changeableGroups.Contains(a.GroupId)).ToListAsync();
+                    var customerNotes = await _context.CustomerNotes.Where(a => changeableGroups.Contains(a.GroupId)).ToListAsync();
+                    var customerSchedulingSoftwareByGroups = await _context.CustomerSchedulingSoftwareByGroup.Where(a => changeableGroups.Contains(a.GroupId)).ToListAsync();
+                    var distributionLogs = await _context.DistributionLog.Where(a => changeableGroups.Contains((a.GroupId ?? 0))).ToListAsync();
+                    var distributionQueues = await _context.DistributionQueue.Where(a => changeableGroups.Contains(a.GroupId)).ToListAsync();
+
+                    users.ForEach(a => a.GroupId = request.BaseGroupId);
+                    await _context.BulkUpdateAsync(users);
+
+                    fbos.ForEach(a => a.GroupId = request.BaseGroupId);
+                    await _context.BulkUpdateAsync(fbos);
+
+                    adminEmails.ForEach(a => a.GroupId = request.BaseGroupId);
+                    await _context.BulkUpdateAsync(adminEmails);
+
+                    companiesByGroups.ForEach(a => a.GroupId = request.BaseGroupId);
+                    await _context.BulkUpdateAsync(companiesByGroups);
+
+                    contactInfoByGroups.ForEach(a => a.GroupId = request.BaseGroupId);
+                    await _context.BulkUpdateAsync(contactInfoByGroups);
+
+                    customerAircrafts.ForEach(a => a.GroupId = request.BaseGroupId);
+                    await _context.BulkUpdateAsync(customerAircrafts);
+
+                    customerAircraftViewedByGroups.ForEach(a => a.GroupId = request.BaseGroupId);
+                    await _context.BulkUpdateAsync(customerAircraftViewedByGroups);
+
+                    customerCompanyTypes.ForEach(a => a.GroupId = request.BaseGroupId);
+                    await _context.BulkUpdateAsync(customerCompanyTypes);
+
+                    customerInfoByGroups.ForEach(a => a.GroupId = request.BaseGroupId);
+                    await _context.BulkUpdateAsync(customerInfoByGroups);
+
+                    customerNotes.ForEach(a => a.GroupId = request.BaseGroupId);
+                    await _context.BulkUpdateAsync(customerNotes);
+
+                    customerSchedulingSoftwareByGroups.ForEach(a => a.GroupId = request.BaseGroupId);
+                    await _context.BulkUpdateAsync(customerSchedulingSoftwareByGroups);
+
+                    distributionLogs.ForEach(a => a.GroupId = request.BaseGroupId);
+                    await _context.BulkUpdateAsync(distributionLogs);
+
+                    distributionQueues.ForEach(a => a.GroupId = request.BaseGroupId);
+                    await _context.BulkUpdateAsync(distributionQueues);
+
+                    var baseCustomerInfoByGroups = _context.CustomerInfoByGroup
+                        .Where(a => a.GroupId == request.BaseGroupId)
+                        .ToList()
+                        .Concat(customerInfoByGroups)
+                        .ToList();
+                    var distinctedByCustomerIDCustomerInfoByGroups = baseCustomerInfoByGroups
+                        .GroupBy(cg => new { cg.CustomerId, cg.GroupId })
+                        .Select(g => g.First())
+                        .ToList();
+                    var duplicatedByCustomerIDCustomerInfoByGroups = baseCustomerInfoByGroups
+                        .Except(distinctedByCustomerIDCustomerInfoByGroups)
+                        .ToList();
+                    await _context.BulkDeleteAsync(duplicatedByCustomerIDCustomerInfoByGroups);
+
+                    var baseContactInfoByGroups = _context.ContactInfoByGroup
+                        .Where(a => a.GroupId == request.BaseGroupId)
+                        .ToList()
+                        .Concat(contactInfoByGroups)
+                        .ToList();
+                    var distinctedContactInfoByGroups = baseContactInfoByGroups
+                        .GroupBy(cg => new { cg.GroupId, cg.Email })
+                        .Select(g => g.First())
+                        .ToList();
+                    var duplicatedContactInfoByGroups = baseContactInfoByGroups
+                        .Except(distinctedContactInfoByGroups)
+                        .ToList();
+                    await _context.BulkDeleteAsync(duplicatedContactInfoByGroups);
+
+                    var groupedByCustomerNameCustomerInfoByGroups = distinctedByCustomerIDCustomerInfoByGroups
+                            .GroupBy(cg => new { Company = cg.Company.ToLower(), cg.GroupId })
+                            .ToList();
+                    groupedByCustomerNameCustomerInfoByGroups.ForEach(async groupedResult =>
+                    {
+                        if (groupedResult.Count() > 0)
+                        {
+                            var customerIds = groupedResult
+                                .Skip(1)
+                                .Select(cg => cg.CustomerId)
+                                .ToList();
+                            var replacingCustomerContacts = _context.CustomerContacts
+                                .Where(c => customerIds.Contains(c.CustomerId))
+                                .ToList();
+                            replacingCustomerContacts.ForEach(c => c.CustomerId = groupedResult.First().CustomerId);
+                            await _context.BulkUpdateAsync(replacingCustomerContacts);
+                            await _context.BulkDeleteAsync(groupedResult.Skip(1).ToList());
+
+                            var duplicatedCustomerAircrafts = _context.CustomerAircrafts.Where(ca => customerIds.Contains(ca.CustomerId)).ToList();
+                            duplicatedCustomerAircrafts.ForEach(ca => ca.CustomerId = groupedResult.First().CustomerId);
+                            await _context.BulkDeleteAsync(duplicatedCustomerAircrafts);
+                        }
+                    });
+
+                    var baseCustomerAircrafts = _context.CustomerAircrafts
+                       .Where(a => a.GroupId == request.BaseGroupId)
+                       .ToList();
+                    var distinctedCustomerAircrafts = baseCustomerAircrafts
+                        .GroupBy(ca => new { ca.GroupId, ca.CustomerId, ca.TailNumber, ca.AircraftId, ca.Size })
+                        .Select(g => g.First())
+                        .ToList();
+                    var duplicatedCustomerAircrafts = baseCustomerAircrafts
+                        .Except(distinctedCustomerAircrafts)
+                        .ToList();
+
+                    duplicatedCustomerAircrafts.ForEach(async ca =>
+                    {
+                        var aircraftPrices = _context.AircraftPrices.Where(ap => ap.CustomerAircraftId == ca.Oid).ToList();
+                        var baseCustomerAircraft = distinctedCustomerAircrafts.Where(dca => dca.GroupId == ca.GroupId
+                            && dca.CustomerId == ca.CustomerId
+                            && dca.TailNumber == ca.TailNumber
+                            && dca.AircraftId == ca.AircraftId
+                            && dca.Size == ca.Size
+                        ).First();
+                        aircraftPrices.ForEach(ap => ap.CustomerAircraftId = baseCustomerAircraft.Oid);
+                        await _context.BulkUpdateAsync(aircraftPrices);
+                    });
+
+                    await _context.BulkDeleteAsync(duplicatedCustomerAircrafts);
+
+                    var removingGroups = _context.Group.Where(a => changeableGroups.Contains(a.Oid));
+                    await _context.BulkDeleteAsync(removingGroups.ToList());
+
+                    transaction.Commit();
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
             }
         }
     }
