77777b87bd738d8e67f2a2fd8092528623f2d53f
robert-the-lawful-head <robertlawhead1@gmail.com>
2022-01-31 22:53:10 +0300

    28dfupn: Antenna data - store the FAA mapped tail in the AirportWatchHistoricalData Table when new records created.

--- FBOLinx.Web/FBOLinx.Web/SQL/28dfupn/ALTER_AirportWatchHistoricalData_ADD_TailNumber.sql
+++ FBOLinx.Web/FBOLinx.Web/SQL/28dfupn/ALTER_AirportWatchHistoricalData_ADD_TailNumber.sql
@@ -6,15 +6,3 @@ ALTER TABLE [AirportWatchHistoricalData]
 ADD [TailNumber] [varchar](25) NULL
 
 GO
-
-DECLARE @FAAAircraftHexTailMapping TABLE ([AircraftHexCode] varchar(25),  [TailNumber] varchar(25));
-
-INSERT @FAAAircraftHexTailMapping SELECT AircraftHexCode,TailNumber FROM Dega.[dbo].[AircraftHexTailMapping];
-
-UPDATE a
-SET a.TailNumber = f.TailNumber
-FROM AirportWatchHistoricalData a
-INNER JOIN @FAAAircraftHexTailMapping f ON f.AircraftHexCode = a.AircraftHexCode
-WHERE isnull(f.TailNumber, '') <> ''
-
-GO
--- FBOLinx.Web/FBOLinx.Web/SQL/28dfupn/ALTER_AirportWatchLiveData_ADD_TailNumber.sql
+++ FBOLinx.Web/FBOLinx.Web/SQL/28dfupn/ALTER_AirportWatchLiveData_ADD_TailNumber.sql
@@ -6,15 +6,3 @@ ALTER TABLE [AirportWatchLiveData]
 ADD [TailNumber] [varchar](25) NULL
 
 GO
-
-DECLARE @FAAAircraftHexTailMapping TABLE ([AircraftHexCode] varchar(25),  [TailNumber] varchar(25));
-
-INSERT @FAAAircraftHexTailMapping SELECT AircraftHexCode,TailNumber FROM Dega.[dbo].[AircraftHexTailMapping];
-
-UPDATE a
-SET a.TailNumber = f.TailNumber
-FROM AirportWatchLiveData a
-INNER JOIN @FAAAircraftHexTailMapping f ON f.AircraftHexCode = a.AircraftHexCode
-WHERE isnull(f.TailNumber, '') <> ''
-
-GO
