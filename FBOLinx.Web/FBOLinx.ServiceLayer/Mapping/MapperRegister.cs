10b8b9b3c70e87a72c7479636ac752eb58e150a6
robert-the-lawful-head <robertlawhead1@gmail.com>
2022-02-15 08:39:27 -0600

    2aj9rw2: create mapster general fro and mapper config

---
+++ FBOLinx.Web/FBOLinx.ServiceLayer/Mapping/MapperRegister.cs
@@ -0,0 +1,20 @@
+﻿using FBOLinx.DB.Models;
+using Mapster;
+using System.Reflection;
+
+namespace FBOLinx.ServiceLayer.Mapping
+{
+    public class MapperRegister : ICodeGenerationRegister
+    {
+        public void Register(CodeGenerationConfig config)
+        {
+            config.AdaptTo("[name]Dto")
+            .ForAllTypesInNamespace(Assembly.Load("FBOLinx.DB"), "FBOLinx.DB.Models")
+             .ExcludeTypes(typeof(FBOLinxBaseEntityModel<>));
+
+            config.GenerateMapper("[name]Mapper")
+            .ForAllTypesInNamespace(Assembly.Load("FBOLinx.DB"), "FBOLinx.DB.Models")
+            .ExcludeTypes(typeof(FBOLinxBaseEntityModel<>));
+        }
+    }
+}
