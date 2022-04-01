68bced588a701eaabee1ade0352d5ce89a216032
robert-the-lawful-head <robertlawhead1@gmail.com>
2022-04-01 02:24:17 -0600

    ui minor issues

--- FBOLinx.Web/FBOLinx.Web/ClientApp/src/app/pages/flight-watch/flight-watch-aircraft-info-dialog/flight-watch-aircraft-info-dialog.component.ts
+++ FBOLinx.Web/FBOLinx.Web/ClientApp/src/app/pages/flight-watch/flight-watch-aircraft-info-dialog/flight-watch-aircraft-info-dialog.component.ts
@@ -1,6 +1,5 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { AnyFn } from '@ngrx/store/src/selector';
 import * as _ from 'lodash';
 import { CustomersListType } from 'src/app/models';
 import { Aircraftwatch, FlightWatch } from 'src/app/models/flight-watch';
