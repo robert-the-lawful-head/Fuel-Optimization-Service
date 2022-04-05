8b9d67716b6b263d2ac6f42d3e4e43f37e66815a
robert-the-lawful-head <robertlawhead1@gmail.com>
2022-04-04 17:39:40 -0700

    Revert "[#2hbncgg] Fixed when specific pricing is the same as standard pricing"
    This reverts commit 784043b8e57750b76b34dc74354faafb2104fb0f.

--- FBOLinx.Web/FBOLinx.ServiceLayer/BusinessServices/PricingTemplate/PricingTemplateService.cs
+++ FBOLinx.Web/FBOLinx.ServiceLayer/BusinessServices/PricingTemplate/PricingTemplateService.cs
@@ -90,15 +90,10 @@ namespace FBOLinx.ServiceLayer.BusinessServices.PricingTemplate
             result.AddRange(standardTemplates);
 
             //Set the applicable tail numbers for the aircraft-specific templates
-            var specificPricingIsStandardPricingCount = 0;
             foreach (DB.Models.PricingTemplate aircraftPricingTemplate in aircraftPricesResult)
             {
                 if (standardTemplates.Any(x => x.Oid == aircraftPricingTemplate.Oid))
-                {
-                    specificPricingIsStandardPricingCount++;
                     continue;
-                }
-
                 var tailNumberList = await _customerAircraftEntityService.GetTailNumbers(aircraftPricingTemplate.Oid, customer.CustomerId, groupId);
  
                 if (tailNumberList == null || tailNumberList.Count == 0)
@@ -108,7 +103,7 @@ namespace FBOLinx.ServiceLayer.BusinessServices.PricingTemplate
                 result.Add(aircraftPricingTemplate);
             }
             //Set the applicable tail numbers for the standard/default templates
-            if (!isAnalytics || (isAnalytics && (aircraftPricesResult.Count != specificPricingIsStandardPricingCount)))
+            if (!isAnalytics || (isAnalytics && aircraftPricesResult.Count > 0))
             {
                 var customerAircrafts = await _customerAircraftEntityService.Where(x => x.CustomerId == customer.CustomerId && x.GroupId == groupId).ToListAsync();
 
