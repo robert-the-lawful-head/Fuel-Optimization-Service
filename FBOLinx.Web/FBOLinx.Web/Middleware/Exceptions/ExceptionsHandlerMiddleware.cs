6940ff18028fa00e1cc5485c28d085eb20f5e9e7
robert-the-lawful-head <robertlawhead1@gmail.com>
2022-04-06 00:04:42 -0500

    create global exception handler

---
+++ FBOLinx.Web/FBOLinx.Web/Middleware/Exceptions/ExceptionsHandlerMiddleware.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace FBOLinx.Web.Middleware.Exceptions
+{
+    public class ExceptionHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionHandlerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next.Invoke(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionMessageAsync(context, ex).ConfigureAwait(false);
+            }
+        }
+        private static Task HandleExceptionMessageAsync(HttpContext context, Exception exception)
+        {
+            context.Response.ContentType = "application/json";
+            int statusCode = (int)HttpStatusCode.InternalServerError;
+            var result = JsonConvert.SerializeObject(new
+            {
+                StatusCode = statusCode,
+                ErrorMessage = exception.Message
+            });
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = statusCode;
+            return context.Response.WriteAsync(result);
+        }
+    }
+}
