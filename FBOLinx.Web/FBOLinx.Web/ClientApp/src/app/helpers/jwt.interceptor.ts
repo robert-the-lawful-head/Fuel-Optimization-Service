8f753f196efe3a5401d7bc3ea8ce9bf82e71977b
robert-the-lawful-head <robertlawhead1@gmail.com>
2022-05-20 13:50:46 -0500

    log currentuser per request on client

--- FBOLinx.Web/FBOLinx.Web/ClientApp/src/app/helpers/jwt.interceptor.ts
+++ FBOLinx.Web/FBOLinx.Web/ClientApp/src/app/helpers/jwt.interceptor.ts
@@ -6,17 +6,20 @@ import {
 } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { SharedService } from '../layouts/shared-service';
 
 import { AuthenticationService } from '../services/authentication.service';
 
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
-    constructor(private authenticationService: AuthenticationService) {}
+    constructor(private authenticationService: AuthenticationService,
+        private sharedService: SharedService) {}
 
     intercept(
         request: HttpRequest<any>,
         next: HttpHandler
     ): Observable<HttpEvent<any>> {
+        console.log("Current User : ",this.sharedService.currentUser);
         // add authorization header with jwt token if available
         const currentUser = this.authenticationService.currentUserValue;
         if (currentUser && currentUser.token) {
