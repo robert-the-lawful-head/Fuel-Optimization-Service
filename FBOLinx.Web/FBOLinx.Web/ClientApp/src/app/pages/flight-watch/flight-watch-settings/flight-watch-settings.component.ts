edc3b0a0ee27ee396a8c3e718e5c4c7474529f55
robert-the-lawful-head <robertlawhead1@gmail.com>
2022-07-19 10:41:15 -0500

    use of table abrtraction on settngs

--- FBOLinx.Web/FBOLinx.Web/ClientApp/src/app/pages/flight-watch/flight-watch-settings/flight-watch-settings.component.ts
+++ FBOLinx.Web/FBOLinx.Web/ClientApp/src/app/pages/flight-watch/flight-watch-settings/flight-watch-settings.component.ts
@@ -1,16 +1,7 @@
-import { ChangeDetectionStrategy, OnInit, ViewChild } from '@angular/core';
+import { ChangeDetectionStrategy, OnInit } from '@angular/core';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatSort, MatSortHeader } from '@angular/material/sort';
-import { MatTableDataSource } from '@angular/material/table';
 import { Observable } from 'rxjs';
 import { Swim } from 'src/app/models/swim';
-import {
-    ColumnType,
-    TableSettingsComponent,
-} from 'src/app/shared/components/table-settings/table-settings.component';
-import { SharedService } from '../../../layouts/shared-service';
 import { FlightWatch } from '../../../models/flight-watch';
 import { AIRCRAFT_IMAGES } from '../flight-watch-map/aircraft-images';
 
@@ -31,120 +22,12 @@ export class FlightWatchSettingsComponent implements OnInit {
     @Output() typesFilterChanged = new EventEmitter<string[]>();
     @Output() filterChanged = new EventEmitter<string>();
     @Output() icaoChanged = new EventEmitter<string>();
-
-
-    @ViewChild(MatSort, { static: true }) sort: MatSort;
-    @ViewChild(MatPaginator) paginator: MatPaginator;
-
-    displayedColumns: string[] = [
-        "flightDepartment",
-        "tailNumber",
-        "make-model",
-        "eta",
-        "ete",
-        "origin-destination",
-        "city",
-        "altitude",
-        "isAircraftOnGround",
-    ];
-
-    tableLocalStorageKey: string;
-    columns: ColumnType[] = [];
+   
     searchIcaoTxt: string;
 
-    constructor(private tableSettingsDialog: MatDialog,
-        private sharedService: SharedService) {
-        this.initColumns();
-    }
+    constructor() {   }
 
-    get visibleColumns() {
-        return (
-            this.columns
-                .filter((column) => !column.hidden)
-                .map((column) => column.id) || []
-        );
-    }
-
-    ngOnInit() {
-        this.sort.sortChange.subscribe(() => {
-            this.columns = this.columns.map((column) =>
-                column.id === this.sort.active
-                ? { ...column, sort: this.sort.direction }
-                : {
-                    hidden: column.hidden,
-                    id: column.id,
-                    name: column.name,
-                }
-            );
-
-            this.saveSettings();
-        });
-    }
-
-    initColumns() {
-        this.tableLocalStorageKey = `flight-watch-settings-${this.sharedService.currentUser.fboId}`;
-        if (localStorage.getItem(this.tableLocalStorageKey)) {
-            this.columns = JSON.parse(
-                localStorage.getItem(this.tableLocalStorageKey)
-            );
-        } else {
-            this.columns = [
-                {
-                    id: 'tailNumber',
-                    name: 'tailNumber',
-                },
-                {
-                    id: 'flightDepartment',
-                    name: 'flightDepartment',
-                    sort: 'desc',
-                },
-                {
-                    id: 'make-model',
-                    name: 'Make/Model',
-                },
-                {
-                    id: 'ete',
-                    name: 'ETE',
-                },
-                {
-                    id: 'eta',
-                    name: 'ETA',
-                },
-                {
-                    id: 'origin-destination',
-                    name: 'Origin/Destination',
-                },
-                {
-                    id: 'city',
-                    name: 'City'
-                },
-                {
-                    id: 'altitude',
-                    name: 'Altitude'
-                },
-                {
-                    id: 'isAircraftOnGround',
-                    name: 'On Ground',
-                },
-                {
-                    id: 'eta-atd',
-                    name: 'ETA/ATD',
-                },
-                {
-                    id: 'itpMarginTemplate',
-                    name: 'ITP Margin Template',
-                },
-                {
-                    id: 'ppg',
-                    name: 'PPG',
-                },
-                {
-                    id: 'fuelCapacityGal',
-                    name: 'Fuel Capacity',
-                }
-            ];
-        }
-    }
+    ngOnInit() {}
 
     get aircraftTypes() {
         return AIRCRAFT_IMAGES.filter((type) => type.label !== 'Other')
@@ -192,53 +75,7 @@ export class FlightWatchSettingsComponent implements OnInit {
             }
         }
     }
-
-    refreshSort() {
-        const sortedColumn = this.columns.find(
-            (column) => !column.hidden && column.sort
-        );
-        this.sort.sort({
-            disableClear: false,
-            id: null,
-            start: sortedColumn?.sort || 'asc',
-        });
-        this.sort.sort({
-            disableClear: false,
-            id: sortedColumn?.id,
-            start: sortedColumn?.sort || 'asc',
-        });
-        (
-            this.sort.sortables.get(sortedColumn?.id) as MatSortHeader
-        )?._setAnimationTransitionState({ toState: 'active' });
-    }
-
-    openSettings() {
-        const dialogRef = this.tableSettingsDialog.open(
-            TableSettingsComponent,
-            {
-                data: this.columns,
-            }
-        );
-        dialogRef.afterClosed().subscribe((result) => {
-            if (!result) {
-                return;
-            }
-
-            this.columns = [...result];
-
-            this.refreshSort();
-            this.saveSettings();
-        });
-    }
-
-    saveSettings() {
-        localStorage.setItem(
-            this.tableLocalStorageKey,
-            JSON.stringify(this.columns)
-        );
-    }
     updateIcao(event: any ){
-        console.log("🚀 ~ file: flight-watch-settings.component.ts ~ line 240 ~ FlightWatchSettingsComponent ~ updateIcao ~ event", event);
         this.icaoChanged.emit(event);
     }
 }
