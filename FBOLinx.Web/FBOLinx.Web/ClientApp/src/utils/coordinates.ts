fa5c4f9a70031e5d59701eddd92c79086b802c29
robert-the-lawful-head <robertlawhead1@gmail.com>
2022-07-27 02:23:52 -0500

    change var to let keyword

--- FBOLinx.Web/FBOLinx.Web/ClientApp/src/utils/coordinates.ts
+++ FBOLinx.Web/FBOLinx.Web/ClientApp/src/utils/coordinates.ts
@@ -1,12 +1,12 @@
 export const convertDMSToDEG = (dms:string): number => {   
-    var dms_Array = dms.split(/[^\d]+/);
+    let dms_Array = dms.split(/[^\d]+/);
 
-    var degrees = dms_Array[1];
-    var minutes = dms_Array[2];
-    var seconds = dms_Array[3];
-    var direction = dms[0];
+    let degrees = dms_Array[1];
+    let minutes = dms_Array[2];
+    let seconds = dms_Array[3];
+    let direction = dms[0];
 
-    var deg = Number((Number(degrees) + Number(minutes)/60 + Number(seconds)/3600).toFixed(6));
+    let deg = Number((Number(degrees) + Number(minutes)/60 + Number(seconds)/3600).toFixed(6));
 
     if (direction == "S" || direction == "W") {
         deg = deg * -1;
