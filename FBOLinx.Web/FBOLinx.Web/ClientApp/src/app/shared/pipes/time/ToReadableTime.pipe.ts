bcab6ecf5e506dcdf6910f33f6710950e471d6e6
robert-the-lawful-head <robertlawhead1@gmail.com>
2022-07-27 02:25:14 -0500

    date and time pipe for readable format string

---
+++ FBOLinx.Web/FBOLinx.Web/ClientApp/src/app/shared/pipes/time/ToReadableTime.pipe.ts
@@ -0,0 +1,12 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'ToReadableTime'
+})
+export class ToReadableTimePipe implements PipeTransform {
+
+  transform(value: any, args?: any): any {
+    value = value.split(/[^-*\d]+/);
+    return value[0]+':'+value[1]+':'+value[2];
+  }
+}
