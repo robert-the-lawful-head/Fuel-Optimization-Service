41b9113b0175d71e2695d307605783e87927d3d3
robert-the-lawful-head <robertlawhead1@gmail.com>
2022-08-10 10:32:41 -0500

    get time from date

--- FBOLinx.Web/FBOLinx.Web/ClientApp/src/app/app-pipes.module.ts
+++ FBOLinx.Web/FBOLinx.Web/ClientApp/src/app/app-pipes.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BooleanToTextPipe } from './shared/pipes/boolean/booleanToText.pipe';
+import { GetTimePipe } from './shared/pipes/dateTime/getTime.pipe';
 import { ToReadableDateTimePipe } from './shared/pipes/dateTime/ToReadableDateTime.pipe';
 import { NullOrEmptyToDefault } from './shared/pipes/null/NullOrEmptyToDefault.pipe';
 import { ToPriceFormatPipe } from './shared/pipes/price/ToPriceFormat.pipe';
@@ -8,8 +9,8 @@ import { SafeHtmlPipe } from './shared/pipes/safe-html-pipe.pipe';
 import { ToReadableTimePipe } from './shared/pipes/time/ToReadableTime.pipe';
 
 @NgModule({
-    declarations: [SafeHtmlPipe,BooleanToTextPipe,NullOrEmptyToDefault,ToPriceFormatPipe,ToReadableDateTimePipe,ToReadableTimePipe],
-    exports: [SafeHtmlPipe,BooleanToTextPipe,NullOrEmptyToDefault,ToPriceFormatPipe,ToReadableDateTimePipe,ToReadableTimePipe],
+    declarations: [SafeHtmlPipe,BooleanToTextPipe,NullOrEmptyToDefault,ToPriceFormatPipe,ToReadableDateTimePipe,ToReadableTimePipe,GetTimePipe],
+    exports: [SafeHtmlPipe,BooleanToTextPipe,NullOrEmptyToDefault,ToPriceFormatPipe,ToReadableDateTimePipe,ToReadableTimePipe,GetTimePipe],
     imports: [CommonModule],
 })
 export class AppPipesModule {}
