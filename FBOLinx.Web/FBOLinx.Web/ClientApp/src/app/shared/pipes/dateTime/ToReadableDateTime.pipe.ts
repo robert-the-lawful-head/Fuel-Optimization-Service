472ed4959278c0e052d4d4301658258ac827963d
robert-the-lawful-head <robertlawhead1@gmail.com>
2022-08-22 11:58:15 -0500

    sorting fix and styling fixes

--- FBOLinx.Web/FBOLinx.Web/ClientApp/src/app/shared/pipes/dateTime/ToReadableDateTime.pipe.ts
+++ FBOLinx.Web/FBOLinx.Web/ClientApp/src/app/shared/pipes/dateTime/ToReadableDateTime.pipe.ts
@@ -1,31 +1,13 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import * as moment from 'moment';
 
 @Pipe({
   name: 'ToReadableDateTime'
 })
 export class ToReadableDateTimePipe implements PipeTransform {
 
-  transform(dateObject: Date, args?: any): string {
+  transform(dateObject: string, args?: any): string {
     if(dateObject == null) return "";
-    var dmy:string[] = [
-        dateObject.getDate().toString(),
-        (dateObject.getMonth() + 1).toString(),
-        dateObject.getFullYear().toString()
-    ];
-    var hms:string[] = [
-        dateObject.getHours().toString(),
-        dateObject.getMinutes().toString(),
-        dateObject.getSeconds().toString()
-    ];
-
-    dmy[0] = ('0' + dmy[0]).slice(-2);
-    dmy[1] = ('0' + dmy[1]).slice(-2);
-    dmy[2] = ('0000' + dmy[2]).slice(-4);
-
-    hms[0] = ('0' + hms[0]).slice(-2);
-    hms[1] = ('0' + hms[1]).slice(-2);
-    hms[2] = ('0' + hms[2]).slice(-2);
-
-    return dmy.join('/') +' '+ hms.join(':');
+    return moment(dateObject).format('L');
   }
 }
