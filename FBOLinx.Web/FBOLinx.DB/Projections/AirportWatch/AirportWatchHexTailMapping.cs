997a8ed284d3ca20231f58e35b746522411c64ee
robert-the-lawful-head <robertlawhead1@gmail.com>
2022-08-26 22:44:12 +0300

    2vkthnz: DB Query Specification Changes (C#): Allow for specifying which columns to include in the generated query by Entity Framework.

---
+++ FBOLinx.Web/FBOLinx.DB/Projections/AirportWatch/AirportWatchHexTailMapping.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FBOLinx.DB.Projections.AirportWatch
+{
+    public class AirportWatchHexTailMapping
+    {
+        public string AircraftHexCode { get; set; }
+        public string TailNumber { get; set; }
+    }
+}
