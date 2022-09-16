36dbade44ace066fed895ee678d479cf90b0df48
robert-the-lawful-head <robertlawhead1@gmail.com>
2022-09-16 19:55:58 +0300

    2vkqqyz: Implementation: Pull aircraft locations using antenna + swims data for a particular flight department

---
+++ FBOLinx.Web/FBOLinx.DB/Specifications/AirportWatchData/AirportWatchHistoricalDataByTailNumberSpecification.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FBOLinx.Core.BaseModels.Specifications;
+using FBOLinx.DB.Models;
+
+namespace FBOLinx.DB.Specifications.AirportWatchData
+{
+    public sealed class AirportWatchHistoricalDataByTailNumberSpecification : Specification<AirportWatchHistoricalData>
+    {
+        public AirportWatchHistoricalDataByTailNumberSpecification(IList<string> tailNumbers, DateTime startDate)
+            : base(x => x.TailNumber != null && tailNumbers.Contains(x.TailNumber) && x.AircraftPositionDateTimeUtc >= startDate)
+        {
+        }
+    }
+}
