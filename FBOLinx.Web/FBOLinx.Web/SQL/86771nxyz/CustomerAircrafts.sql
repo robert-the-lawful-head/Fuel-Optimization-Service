274b5e2ea65d255d1b3c18df22469f9fce132fc3
robert-the-lawful-head <robertlawhead1@gmail.com>
2023-01-09 14:12:56 -0800

    [#86771nxyz] Query to clean up duplicate CustomerAircrafts, update deleted acData2 tails in FBOLinx

--- FBOLinx.Web/FBOLinx.Web/SQL/86771nxyz/CustomerAircrafts.sql
+++ FBOLinx.Web/FBOLinx.Web/SQL/86771nxyz/CustomerAircrafts.sql
@@ -1,16 +1,22 @@
-USE [paragon_test]
+[use] paragon_Test
 
-WITH cte AS (
-    SELECT 
-      groupid, customerid, tailnumber,
-        ROW_NUMBER() OVER (
-            PARTITION BY 
-                 groupid, customerid, tailnumber
-            ORDER BY 
-               groupid, customerid, tailnumber
-        ) row_num
-     FROM 
-        customeraircrafts
-)
-DELETE FROM cte
-WHERE row_num > 1;
+delete caDelete
+from CustomerAircrafts caDelete
+WHERE OID NOT IN
+(
+
+select min(ca.oid) 
+
+from CustomerAircrafts ca 
+group by ca.GroupID, ca.customerid, tailnumber
+) 
+
+
+
+update ca
+set customerid=0
+from customeraircrafts ca
+inner join customers c on c.oid=ca.customerid
+inner join sqlfl.dbo.companies co on co.oid=c.fuelerlinxid
+left join sqlfl.dbo.acdata2 ac on ac.tailno=ca.tailnumber
+where ac.oid is null and ca.groupid>1 and ca.addedfrom=1
