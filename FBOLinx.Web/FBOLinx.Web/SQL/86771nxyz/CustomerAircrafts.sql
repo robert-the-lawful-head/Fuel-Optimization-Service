e9b3e290b0a186494787fe5db6e9e39922946e88
robert-the-lawful-head <robertlawhead1@gmail.com>
2023-01-10 14:39:17 -0800

    [#86771nxyz] Adjusted query, and added in Mike's delete query

--- FBOLinx.Web/FBOLinx.Web/SQL/86771nxyz/CustomerAircrafts.sql
+++ FBOLinx.Web/FBOLinx.Web/SQL/86771nxyz/CustomerAircrafts.sql
@@ -1,15 +1,46 @@
 [use] paragon_Test
+go
 
-delete caDelete
-from CustomerAircrafts caDelete
-WHERE OID NOT IN
-(
+select ca.* 
+into #AircraftWithAssignedPricing
+from customeraircrafts ca
+inner join aircraftprices ap on ap.customeraircraftid=ca.oid and ca.groupid>1
+inner join pricingtemplate pt on pt.oid=ap.PriceTemplateID
 
-select min(ca.oid) 
 
-from CustomerAircrafts ca 
-group by ca.GroupID, ca.customerid, tailnumber
-) 
+--Add all customer aircraft to the #ResultsForEvaluation temp table.  
+--The MinimumValidCustomerAircraftID column should be the assigned AircraftPrices CustomerAircraftID if available.  Otherwise, the minimum OID of the customerAircrafts record.
+select min(ap.oid) as MinAssignedPricingOID,
+	max(ap.oid) as MaxAssignedPricingOID,
+	min(case when ap.oid > 0 then ap.oid else ca.oid end) as MinimumValidCustomerAircraftID,
+	ca.GroupID,
+	ca.CustomerID,
+	ca.TailNumber	
+into #ResultsForEvaluation
+from CustomerAircrafts ca
+left join #AircraftWithAssignedPricing ap on ap.OID = ca.oid
+where ca.GroupID > 1
+group by ca.GroupID, ca.customerid, ca.tailnumber
+
+
+--Results that require an evaluation because a pricing was somehow assigned to multiple copies (CustomerAircraft records) of the tail.  Should be handled first and corrected manually.
+select *
+from #ResultsForEvaluation re
+where re.MinAssignedPricingOID <> MaxAssignedPricingOID
+
+
+
+--Results that should be OK to delete after the ABOVE is cleaned up - PLEASE DOUBLE-CHECK SOME EXAMPLES FROM THIS LIST.  
+delete ca
+from CustomerAircrafts ca
+left join #ResultsForEvaluation re on re.MinimumValidCustomerAircraftID = ca.oid
+where ca.GroupID > 1
+and re.MinimumValidCustomerAircraftID is null
+
+
+
+drop table #AircraftWithAssignedPricing
+drop table #ResultsForEvaluation
 
 
 
@@ -19,4 +50,5 @@ from customeraircrafts ca
 inner join customers c on c.oid=ca.customerid
 inner join sqlfl.dbo.companies co on co.oid=c.fuelerlinxid
 left join sqlfl.dbo.acdata2 ac on ac.tailno=ca.tailnumber
-where ac.oid is null and ca.groupid>1 and ca.addedfrom=1
+left join sqlfl.dbo.useraircraft ua on ua.tailnumid=ac.oid
+where (ac.oid is null or ua.oid is null) and ca.groupid>1
