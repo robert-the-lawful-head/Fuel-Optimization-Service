b7d27fc5fcd2d1f6bfe556c288aa2a583aae06b4
robert-the-lawful-head <robertlawhead1@gmail.com>
2023-02-21 11:12:45 -0600

    crete csv fiel opts interface

--- FBOLinx.Web/FBOLinx.Web/ClientApp/src/app/services/tables/VirtualScrollBase.ts
+++ FBOLinx.Web/FBOLinx.Web/ClientApp/src/app/services/tables/VirtualScrollBase.ts
@@ -3,6 +3,12 @@ import { MatSort } from "@angular/material/sort";
 import { MatTableDataSource } from "@angular/material/table";
 import { ColumnType } from "src/app/shared/components/table-settings/table-settings.component";
 import * as XLSX from 'xlsx';
+
+export type csvFileOptions = {
+    fileName: string;
+    sheetName: string;
+}
+
 export abstract class VirtualScrollBase {
     start: number = 0;
     limit: number = 10;
