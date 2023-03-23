8942ae69c959e4b9abcb5ed9fa79d88a531153cc
robert-the-lawful-head <robertlawhead1@gmail.com>
2023-03-23 16:08:06 -0600

    customer notes not displaying on grid fix

--- FBOLinx.Web/FBOLinx.ServiceLayer/DTO/FuelReqDto.g.cs
+++ FBOLinx.Web/FBOLinx.ServiceLayer/DTO/FuelReqDto.g.cs
@@ -33,7 +33,7 @@ namespace FBOLinx.Service.Mapping.Dto
         public string PhoneNumber { get; set; }
         public string FuelOn { get; set; } = "Departure";
         public string CustomerName { get; set; }
-        public string CustomerOrderNotes { get; set; }
+        public string CustomerNotes { get; set; }
         public string PaymentMethod { get; set; }
         public string TimeZone { get; set; }
         public string TailNumber
