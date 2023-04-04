3f681d6495d7038e0c1a922a11b41da9fa606952
robert-the-lawful-head <robertlawhead1@gmail.com>
2023-04-04 16:13:34 -0700

    [#8677qtygd] Added a few more drop indexes

--- FBOLinx.Web/FBOLinx.Web/SQL/8677qtygd/CustomerAircrafts.sql
+++ FBOLinx.Web/FBOLinx.Web/SQL/8677qtygd/CustomerAircrafts.sql
@@ -4,6 +4,12 @@ DROP INDEX CustomerAircrafts.INX_GroupID;
 
 GO
 
+DROP INDEX [INX_CustomerAircrafts_TailNumber] ON [paragon_test].[dbo].[CustomerAircrafts];
+GO
+
+DROP INDEX [INX_CA_ID] ON [paragon_test].[dbo].[CustomerAircrafts];
+GO
+
 CREATE NONCLUSTERED INDEX [indx_DistributionQueue_GroupID_FBOID_CustomerID] ON [dbo].[DistributionQueue]
 (
 	[GroupID] ASC,
@@ -11,5 +17,4 @@ CREATE NONCLUSTERED INDEX [indx_DistributionQueue_GroupID_FBOID_CustomerID] ON [
 	[CustomerID] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 GO
-
 
