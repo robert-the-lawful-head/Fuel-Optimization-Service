c2aacd34f4c535f1e7580c97d06bc0aa4eb7aa2d
robert-the-lawful-head <robertlawhead1@gmail.com>
2023-05-03 14:08:59 -0700

    [#8677kuhf7] Adjusted the specification for fetching customer aircrafts by group. Chau will refactor further in a follow-up project

--- FBOLinx.Web/FBOLinx.DB/Specifications/CustomerInfoByGroup/CustomerInfoByGroupByFboIdSpecification.cs
+++ FBOLinx.Web/FBOLinx.DB/Specifications/CustomerInfoByGroup/CustomerInfoByGroupByFboIdSpecification.cs
@@ -13,7 +13,7 @@ namespace FBOLinx.DB.Specifications.CustomerInfoByGroup
         {
             AddInclude(x => x.Customer);
             AddInclude(x => x.Customer.CustomCustomerType.Where(x => x.Fboid == fboId));
-            AddInclude(x => x.Customer.CustomerContacts);
+            //AddInclude(x => x.Customer.CustomerContacts);
         }
     }
 }
