fa6fc51682faae3adaf27c636768239fe6f79505
robert-the-lawful-head <robertlawhead1@gmail.com>
2023-05-18 08:54:38 -0600

    disable db retry on failure execution

--- FBOLinx.Web/FBOLinx.DB/Extensions/DbServiceCollectionExtension.cs
+++ FBOLinx.Web/FBOLinx.DB/Extensions/DbServiceCollectionExtension.cs
@@ -22,33 +22,33 @@ namespace FBOLinx.DB.Extensions
         {
             if (!string.IsNullOrEmpty(degaDbConnectionString))
                 services.AddDbContext<DegaContext>(options => {
-                    options.UseSqlServer(degaDbConnectionString,
-                        sqlServerOptions => sqlServerOptions.EnableRetryOnFailure(maxRetryCount: 1));                    
+                    options.UseSqlServer(degaDbConnectionString);
+                        //sqlServerOptions => sqlServerOptions.EnableRetryOnFailure(maxRetryCount: 1));                    
                 });
 
             if (!string.IsNullOrEmpty(fbolinxDbConnectionString))
                 services.AddDbContext<FboLinxContext>(options => {
-                    options.UseSqlServer(fbolinxDbConnectionString,
-                        sqlServerOptions => sqlServerOptions.EnableRetryOnFailure(maxRetryCount: 1));
+                    options.UseSqlServer(fbolinxDbConnectionString);
+                        //sqlServerOptions => sqlServerOptions.EnableRetryOnFailure(maxRetryCount: 1));
                 });
 
             if (!string.IsNullOrEmpty(fuelerlinxDbConnectionString))
                 services.AddDbContext<FuelerLinxContext>(options => {
-                    options.UseSqlServer(fuelerlinxDbConnectionString,
-                        sqlServerOptions => sqlServerOptions.EnableRetryOnFailure(maxRetryCount: 1));
+                    options.UseSqlServer(fuelerlinxDbConnectionString);
+                        //sqlServerOptions => sqlServerOptions.EnableRetryOnFailure(maxRetryCount: 1));
                 });
 
             if (!string.IsNullOrEmpty(fileStorageDbConnectionString))
                 services.AddDbContext<FilestorageContext>(options => {
-                    options.UseSqlServer(fileStorageDbConnectionString,
-                        sqlServerOptions => sqlServerOptions.EnableRetryOnFailure(maxRetryCount: 1));
+                    options.UseSqlServer(fileStorageDbConnectionString);
+                        //sqlServerOptions => sqlServerOptions.EnableRetryOnFailure(maxRetryCount: 1));
                     options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
                 });
 
             if (!string.IsNullOrEmpty(serviceLogsDbConnectionString))
                 services.AddDbContext<ServiceLogsContext>(options => {
-                    options.UseSqlServer(serviceLogsDbConnectionString,
-                        sqlServerOptions => sqlServerOptions.EnableRetryOnFailure(maxRetryCount: 1));
+                    options.UseSqlServer(serviceLogsDbConnectionString);
+                        //sqlServerOptions => sqlServerOptions.EnableRetryOnFailure(maxRetryCount: 1));
                     options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
                 });
 
