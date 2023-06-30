88e2256a4178f8dc74d9b3e6b1829236f64706cf
robert-the-lawful-head <robertlawhead1@gmail.com>
2023-06-30 05:20:03 -0600

    crud operations for fbo custom services and fees

--- FBOLinx.Web/FBOLinx.ServiceLayer/DTO/ServicesAndFees/ServicesAndFeesDto.cs
+++ FBOLinx.Web/FBOLinx.ServiceLayer/DTO/ServicesAndFees/ServicesAndFeesDto.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace FBOLinx.ServiceLayer.DTO.ServicesAndFees
+﻿namespace FBOLinx.ServiceLayer.DTO.ServicesAndFees
 {
     public class ServicesAndFeesDto
     {
-        public string Oid { get; set; }
+        public int Oid { get; set; }
+        public int? HandlerId { get; set; }
+        public int? ServiceOfferedId { get; set; }
         public string Service { get; set; }
         public string ServiceType { get; set; }
     }
