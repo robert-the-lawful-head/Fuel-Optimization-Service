36ee6044a61d676247cbb7e0e74752c46b9b0ae3
robert-the-lawful-head <robertlawhead1@gmail.com>
2023-07-11 10:58:43 -0600

    ramp fees and service and fees components under home  in accordions

---
+++ FBOLinx.Web/FBOLinx.Web/ClientApp/src/app/services/servicetypes.service.ts
@@ -0,0 +1,40 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ServiceTypeResponse } from '../models/services-and-fees/services-and-fees';
+
+@Injectable()
+export class ServiceTypeService {
+    private headers: HttpHeaders;
+    private accessPointUrl: string;
+
+    constructor(private http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+        this.headers = new HttpHeaders({
+            'Content-Type': 'application/json; charset=utf-8',
+        });
+        this.accessPointUrl = baseUrl + 'api/FboCustomServiceTypes';
+    }
+    public getServiceTypes(fboId : number) : Observable<ServiceTypeResponse[]> {
+        return this.http.get<ServiceTypeResponse[]>(this.accessPointUrl + '/fbo/' +fboId, {
+            headers: this.headers,
+        });
+    }
+
+    public add(fboId: number , payload: ServiceTypeResponse) : Observable<ServiceTypeResponse> {
+        return this.http.post<ServiceTypeResponse>(this.accessPointUrl + '/fbo/' + fboId, payload, {
+            headers: this.headers,
+        });
+    }
+
+    public remove(serviceTypeId: number) : Observable<void>{
+        return this.http.delete<void>(this.accessPointUrl + '/' + serviceTypeId, {
+            headers: this.headers,
+        });
+    }
+
+    public update(fboId: number, payload:ServiceTypeResponse) : Observable<ServiceTypeResponse> {
+        return this.http.put<ServiceTypeResponse>(this.accessPointUrl + '/fbo/' + fboId, payload, {
+            headers: this.headers,
+        });
+    }
+}
