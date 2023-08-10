d45995aef442676035dca5eb7fea9975a55ca0df
robert-the-lawful-head <robertlawhead1@gmail.com>
2023-08-10 14:19:46 -0600

    create favorites aircraft and companies service interface implementation wit no logic

---
+++ FBOLinx.Web/FBOLinx.ServiceLayer/BusinessServices/Favorites/FboAircraftFavoritesService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FBOLinx.DB.Context;
+using FBOLinx.DB.Models;
+using FBOLinx.ServiceLayer.EntityServices;
+
+namespace FBOLinx.ServiceLayer.BusinessServices.Favorites
+{
+    public interface IFboCompaniesFavoritesService
+    {
+        Task<FboFavoriteCompanies> AddCompanyFavorite(int fboId, int CustomerInfoByGroupId);
+        Task<bool> DeleteCompanyFavorite(int oid);
+        Task<List<FboFavoriteCompanies>> GetCompaniesFavoritesByFboId(int fboId);
+    }
+
+    public class FboCompaniesFavoritesService : IFboAircraftFavoritesService
+    {
+        private IRepository<FboFavoriteCompanies, FboLinxContext> _fboFavoriteCompaniesRepo;
+
+        public FboCompaniesFavoritesService(IRepository<FboFavoriteCompanies, FboLinxContext> fboFavoriteCompaniesRepo)
+        {
+            _fboFavoriteCompaniesRepo = fboFavoriteCompaniesRepo;
+        }
+
+        public Task<FboFavoriteAircrafts> AddAircraftFavorite(int fboId, int aircraftId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<FboFavoriteCompanies> AddCompanyFavorite(int fboId, int CustomerInfoByGroupId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteAircraftFavorite(int oid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteCompanyFavorite(int oid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<FboFavoriteAircrafts>> GetAircraftFavoriteByFboId(int fboId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<FboFavoriteCompanies>> GetCompaniesFavoritesByFboId(int fboId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FBOLinx.Web/FBOLinx.ServiceLayer/BusinessServices/Favorites/FboCompaniesFavoritesService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FBOLinx.DB.Context;
+using FBOLinx.DB.Models;
+using FBOLinx.ServiceLayer.EntityServices;
+
+namespace FBOLinx.ServiceLayer.BusinessServices.Favorites
+{
+    public interface IFboAircraftFavoritesService
+    {
+        Task<FboFavoriteAircrafts> AddAircraftFavorite(int fboId, int aircraftId);
+        Task<bool> DeleteAircraftFavorite(int oid);
+        Task<List<FboFavoriteCompanies>> GetCompaniesFavoritesByFboId(int fboId);
+    }
+
+    public class FboAircraftFavoritesService : IFboAircraftFavoritesService
+    {
+        private IRepository<FboFavoriteAircrafts, FboLinxContext> _fboFavoriteAircraftsRepo;
+
+        public FboAircraftFavoritesService(IRepository<FboFavoriteAircrafts, FboLinxContext> fboFavoriteAircraftsRepo)
+        {
+            _fboFavoriteAircraftsRepo = fboFavoriteAircraftsRepo;
+        }
+
+        public Task<FboFavoriteAircrafts> AddAircraftFavorite(int fboId, int aircraftId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteAircraftFavorite(int oid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<FboFavoriteCompanies>> GetCompaniesFavoritesByFboId(int fboId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
