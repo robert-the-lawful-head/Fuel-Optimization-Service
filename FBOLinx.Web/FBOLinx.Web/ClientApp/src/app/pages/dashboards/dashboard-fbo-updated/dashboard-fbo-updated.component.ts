e408da7504c2ee272790be6b68e0b480372cbede
robert-the-lawful-head <robertlawhead1@gmail.com>
2023-08-29 00:51:53 -0600

    use flight watch value change subscription to update map on dashboard

--- FBOLinx.Web/FBOLinx.Web/ClientApp/src/app/pages/dashboards/dashboard-fbo-updated/dashboard-fbo-updated.component.ts
+++ FBOLinx.Web/FBOLinx.Web/ClientApp/src/app/pages/dashboards/dashboard-fbo-updated/dashboard-fbo-updated.component.ts
@@ -3,8 +3,7 @@ import { Router } from '@angular/router';
 import { Dictionary } from 'lodash';
 import { LngLatLike } from 'mapbox-gl';
 import * as moment from 'moment';
-import { Subscription, timer } from 'rxjs';
-import { ApiResponseWraper } from 'src/app/models/apiResponseWraper';
+import { Subscription } from 'rxjs';
 import { FlightWatchModelResponse } from 'src/app/models/flight-watch';
 
 // Services
@@ -13,7 +12,6 @@ import * as SharedEvents from '../../../models/sharedEvents';
 import { StatisticsOrdersByLocationComponent } from '../../../shared/components/statistics-orders-by-location/statistics-orders-by-location.component';
 import { StatisticsTotalAircraftComponent } from '../../../shared/components/statistics-total-aircraft/statistics-total-aircraft.component';
 import { StatisticsTotalCustomersComponent } from '../../../shared/components/statistics-total-customers/statistics-total-customers.component';
-import { FlightWatchService } from 'src/app/services/flightwatch.service';
 // Components
 import { StatisticsTotalOrdersComponent } from '../../../shared/components/statistics-total-orders/statistics-total-orders.component';
 import { FlightWatchMapService } from '../../flight-watch/flight-watch-map/flight-watch-map-services/flight-watch-map.service';
@@ -46,7 +44,6 @@ export class DashboardFboUpdatedComponent implements AfterViewInit, OnDestroy {
     //flghtWatch
     center: LngLatLike;
     flightWatchDictionary: Dictionary<FlightWatchModelResponse>;
-    airportWatchFetchSubscription: Subscription;
     mapLoadSubscription: Subscription;
     isMapLoading: boolean = true;
     isStable: boolean = true;
@@ -54,7 +51,6 @@ export class DashboardFboUpdatedComponent implements AfterViewInit, OnDestroy {
 
     constructor(private sharedService: SharedService,
         private router: Router,
-        private flightWatchService: FlightWatchService,
         private flightWatchMapService: FlightWatchMapService,
         ) {
         this.filterStartDate = new Date(
@@ -86,7 +82,6 @@ export class DashboardFboUpdatedComponent implements AfterViewInit, OnDestroy {
             });
         }
         this.selectedICAO = this.sharedService.getCurrentUserPropertyValue(localStorageAccessConstant.icao);
-
     }
 
     get isCsr() {
@@ -98,9 +93,20 @@ export class DashboardFboUpdatedComponent implements AfterViewInit, OnDestroy {
     }
     async ngOnInit() {
         await this.setFlightWatchMapCenter();
-        this.mapLoadSubscription = timer(0, 15000).subscribe(() =>{
-            if(this.selectedICAO)
-                this.loadAirportWatchData();
+
+        this.sharedService.valueChanged$.subscribe((value: {event: string, data: any}) => {
+            if(value.event === SharedEvents.flightWatchDataEvent){
+                if(value.data =! null){
+                    this.flightWatchDictionary = this.flightWatchMapService.getDictionaryByTailNumberAsKey(
+                        value.data
+                    );
+                    this.isStable = true;
+                }else{
+                    this.flightWatchDictionary = null;
+                    this.isStable = false;
+                }
+                this.isMapLoading = false;
+            }
         });
     }
 
@@ -109,9 +115,6 @@ export class DashboardFboUpdatedComponent implements AfterViewInit, OnDestroy {
             this.sharedService.changeEmitted$.subscribe((message) => {
                 if (message === SharedEvents.locationChangedEvent) {
                     this.applyDateFilterChange();
-
-                    this.isMapLoading = true;
-                    this.loadAirportWatchData();
                 }else if(message == SharedEvents.icaoChangedEvent){
                     this.selectedICAO = this.sharedService.getCurrentUserPropertyValue(localStorageAccessConstant.icao);
                     this.setFlightWatchMapCenter();
@@ -134,28 +137,6 @@ export class DashboardFboUpdatedComponent implements AfterViewInit, OnDestroy {
         this.statisticsTotalCustomers.refreshData();
         this.statisticsTotalAircraft.refreshData();
         this.statisticsOrdersByLocation.refreshData();
-    }
-    loadAirportWatchData() {
-        return this.airportWatchFetchSubscription = this.flightWatchService
-        .getAirportLiveData(
-            this.sharedService.currentUser.fboId,
-            this.selectedICAO
-        )
-        .subscribe((data: ApiResponseWraper<FlightWatchModelResponse[]>) => {
-            if (data.success) {
-                this.flightWatchDictionary = this.flightWatchMapService.getDictionaryByTailNumberAsKey(
-                    data.result
-                );
-                this.isStable = true;
-            } else {
-                this.flightWatchDictionary = null;
-                this.isStable = false;
-            }
-            this.isMapLoading = false;
-        }, (error: any) => {
-            this.isMapLoading = false;
-            this.isStable = false;
-        });
     }
 
     gotoFlightWatch(){
