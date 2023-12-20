65317882dbb50111fc4985111ecf2e00949ed489
robert-the-lawful-head <robertlawhead1@gmail.com>
2023-12-20 03:31:50 -0600

    remove bradcrumbs and show only title

--- FBOLinx.Web/FBOLinx.Web/ClientApp/src/app/ni-components/ni-breadcrumb/ni-breadcrumb.component.ts
+++ FBOLinx.Web/FBOLinx.Web/ClientApp/src/app/ni-components/ni-breadcrumb/ni-breadcrumb.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, SimpleChanges } from '@angular/core';
 
 import { Item } from './item';
 
@@ -11,5 +11,20 @@ export class NiBreadcrumbComponent {
     @Input() menu: Item[] = [];
     @Input() separator = '/';
 
+    private _sectionTitle: Item  = { title: 'None', icon: '', link:'' };
     constructor() {}
+
+    ngOnChanges(changes: SimpleChanges): void {
+        if(changes.menu) {
+            this._sectionTitle = changes.menu.currentValue.pop();
+        }
+    }
+
+    get sectionTitle(): Item {
+        return this._sectionTitle;
+
+    }
+    set sectionTitle(title: Item) {
+        this._sectionTitle = title;
+    }
 }
