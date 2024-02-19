088a046b1a033789a9acd7db937d14e33580190c
robert-the-lawful-head <robertlawhead1@gmail.com>
2024-02-19 12:19:19 -0800

    [#AFOSN] Changed delay

--- FBOLinx.Web/FBOLinx.Web/ClientApp/src/app/pages/fuelreqs/fuelreqs-grid/fuelreqs-grid.component.ts
+++ FBOLinx.Web/FBOLinx.Web/ClientApp/src/app/pages/fuelreqs/fuelreqs-grid/fuelreqs-grid.component.ts
@@ -569,7 +569,6 @@ export class FuelreqsGridComponent extends GridBase implements OnInit, OnChanges
         await this.sleep(100);
 
         if (isManuallyClosed && this.openedNotes.length > 0) {
-            await this.sleep(300);
             if (this.openedNotes.length > 0) {
                 const index = this.openedNotes.indexOf(elementId);
                 if (index === -1) {
@@ -581,6 +580,7 @@ export class FuelreqsGridComponent extends GridBase implements OnInit, OnChanges
                 this.openedNotes.splice(index, 1);
 
                 if (this.openedNotes.length > 0) {
+                    await this.sleep(400);
                     var lastOrder = this.openedNotes[this.openedNotes.length - 1];
                     var lastOrderInfo = lastOrder.split("|");
                     var changes = { serviceOrderId: lastOrderInfo[0], associatedFuelOrderId: lastOrderInfo[1], fuelerLinxTransactionId: lastOrderInfo[2], tailNumber: lastOrderInfo[3], customerId: lastOrderInfo[4], customer: lastOrderInfo[5], isDrawerManuallyClicked: true };
