133351c79b9dad0768529b2dde8166f00fbf87db
robert-the-lawful-head <robertlawhead1@gmail.com>
2024-03-02 02:19:07 -0600

    calculate offset and limit of dates on click on preset  date filter component

--- FBOLinx.Web/FBOLinx.Web/ClientApp/src/app/shared/components/preset-date-filter/preset-date-filter.component.ts
+++ FBOLinx.Web/FBOLinx.Web/ClientApp/src/app/shared/components/preset-date-filter/preset-date-filter.component.ts
@@ -1,11 +1,23 @@
-import { Component, EventEmitter, Input, OnInit, Output, SimpleChanges } from '@angular/core';
+import {
+    Component,
+    EventEmitter,
+    Input,
+    OnInit,
+    Output,
+    SimpleChanges,
+} from '@angular/core';
 
 export enum PresetDateFilterEnum {
-    sevenDays = "7D",
-    oneMonth = "1M",
-    sixMonths = "6M",
-    oneYear = "1Y",
-    currentYearToDate = "YTD",
+    sevenDays = '7D',
+    oneMonth = '1M',
+    sixMonths = '6M',
+    oneYear = '1Y',
+    currentYearToDate = 'YTD',
+}
+export interface SelectedPresetDateFilter {
+    selectedFilter: PresetDateFilterEnum;
+    offsetDate: Date;
+    limitDate: Date;
 }
 @Component({
     selector: 'app-preset-date-filter',
@@ -14,10 +26,13 @@ export enum PresetDateFilterEnum {
 })
 export class PresetDateFilterComponent implements OnInit {
     @Input() defaultFilter: PresetDateFilterEnum;
-    @Output() selectedFilterChange = new EventEmitter<PresetDateFilterEnum>();
+    @Output() selectedFilterChange = new EventEmitter<SelectedPresetDateFilter>();
 
     public selectedFilter: PresetDateFilterEnum;
     dateFilterValues = Object.values(PresetDateFilterEnum) as string[];
+
+    currentDate: Date = new Date();
+    startDate: Date = new Date();
 
     constructor() {}
 
@@ -31,6 +46,47 @@ export class PresetDateFilterComponent implements OnInit {
     }
 
     onToggleChange(event: any) {
-        this.selectedFilterChange.emit(event.value);
+        let filter = this.getPresetDatesFilters(event.value);
+        this.selectedFilterChange.emit(filter);
+    }
+    private getPresetDatesFilters(value: PresetDateFilterEnum): SelectedPresetDateFilter {
+        this.startDate = new Date(this.currentDate);
+        switch (value) {
+            case PresetDateFilterEnum.sevenDays:
+                this.calculateDateMinusDays(7);
+                break;
+            case PresetDateFilterEnum.oneMonth:
+                this.calculateDateMinusMonth(1);
+                break;
+            case PresetDateFilterEnum.sixMonths:
+                this.calculateDateMinusMonth(6);
+                break;
+            case PresetDateFilterEnum.oneYear:
+                this.calculateDateMinusYears(1);
+                break;
+            case PresetDateFilterEnum.currentYearToDate:
+                this.calculateFirstDayOfCurrentYear();
+                break;
+        }
+
+        return {
+            selectedFilter: value,
+            offsetDate: this.startDate,
+            limitDate: this.currentDate,
+        }
+    }
+    private calculateDateMinusDays(days: number) {
+        this.startDate.setDate(this.currentDate.getDate() - days);
+    }
+
+    private calculateDateMinusMonth(months: number) {
+        this.startDate.setMonth(this.currentDate.getMonth() - months);
+    }
+
+    private calculateDateMinusYears(years: number) {
+        this.startDate.setFullYear(this.currentDate.getFullYear() - years);
+    }
+    private calculateFirstDayOfCurrentYear() {
+        this.startDate = new Date(this.currentDate.getFullYear(), 0, 1);
     }
 }
