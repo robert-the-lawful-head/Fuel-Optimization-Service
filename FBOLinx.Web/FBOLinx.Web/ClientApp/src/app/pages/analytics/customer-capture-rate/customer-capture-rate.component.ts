c7ca117cf9415ae58ecae47b4751d1b9b4d5bba1
robert-the-lawful-head <robertlawhead1@gmail.com>
2024-03-11 00:40:30 -0600

    capture rate export data with soprted columns

--- FBOLinx.Web/FBOLinx.Web/ClientApp/src/app/pages/analytics/customer-capture-rate/customer-capture-rate.component.ts
+++ FBOLinx.Web/FBOLinx.Web/ClientApp/src/app/pages/analytics/customer-capture-rate/customer-capture-rate.component.ts
@@ -27,14 +27,15 @@ export class CustomerCaptureRateComponent extends GridBase implements OnInit {
     fboId: number;
     fbo: string;
     icao: string;
-    reportHiddenItems: ReportFilterItems[] = [
-        ReportFilterItems.icaoDropDown
-    ];
+    reportHiddenItems: ReportFilterItems[] = [ReportFilterItems.icaoDropDown];
     tableLocalStorageKey: string;
 
     chartName = 'customer-capture-rate-table';
 
-    csvFileOptions: csvFileOptions = { fileName: 'Customer Capture Rate', sheetName: 'Customer Capture Rate ' };
+    csvFileOptions: csvFileOptions = {
+        fileName: 'Customer Capture Rate',
+        sheetName: 'Customer Capture Rate ',
+    };
 
     columns: ColumnType[] = [
         {
@@ -51,20 +52,34 @@ export class CustomerCaptureRateComponent extends GridBase implements OnInit {
         },
         {
             id: 'percentCustomerBusiness',
-            name: '% of Customer\'s Business',
-        }
+            name: "% of Customer's Business",
+        },
     ];
 
-    constructor(private fuelreqsService: FuelreqsService,
+    constructor(
+        private fuelreqsService: FuelreqsService,
         private sharedService: SharedService,
         private tableSettingsDialog: MatDialog,
-        private ngxLoader: NgxUiLoaderService) {
+        private ngxLoader: NgxUiLoaderService
+    ) {
         super();
         this.tableLocalStorageKey = `analytics-customer-capture-rate-${this.sharedService.currentUser.fboId}`;
-        this.groupId = Number(this.sharedService.getCurrentUserPropertyValue(localStorageAccessConstant.groupId));
-        this.fboId = Number(this.sharedService.getCurrentUserPropertyValue(localStorageAccessConstant.fboId));
-        this.fbo = this.sharedService.getCurrentUserPropertyValue(localStorageAccessConstant.fbo);
-        this.icao = this.sharedService.getCurrentUserPropertyValue(localStorageAccessConstant.icao);
+        this.groupId = Number(
+            this.sharedService.getCurrentUserPropertyValue(
+                localStorageAccessConstant.groupId
+            )
+        );
+        this.fboId = Number(
+            this.sharedService.getCurrentUserPropertyValue(
+                localStorageAccessConstant.fboId
+            )
+        );
+        this.fbo = this.sharedService.getCurrentUserPropertyValue(
+            localStorageAccessConstant.fbo
+        );
+        this.icao = this.sharedService.getCurrentUserPropertyValue(
+            localStorageAccessConstant.icao
+        );
 
         this.tableLocalStorageKey = `analytics-companies-quotes-deal-${this.sharedService.currentUser.fboId}`;
         this.columns = this.getClientSavedColumns(
@@ -80,41 +95,114 @@ export class CustomerCaptureRateComponent extends GridBase implements OnInit {
             selectedFilter: null,
             offsetDate: this.filterStartDate,
             limitDate: this.filterEndDate,
-        }
+        };
         this.refreshData();
     }
-    ngOnInit(): void {
-    }
+    ngOnInit(): void {}
     refreshData() {
         let endDate = this.getEndOfDayTime(this.filterEndDate, true);
         let startDate = this.getStartOfDayTime(this.filterStartDate, true);
 
         this.ngxLoader.startLoader(this.chartName);
-        this.fuelreqsService.getCustomerCaptureRate(this.groupId,this.fboId, startDate, endDate).subscribe((data: CustomerCaptureRateReport[]) => {
-                this.dataSource.data = data;
-                this.dataSource.sort = this.sort;
-                this.paginator.pageIndex = 0;
-                this.dataSource.paginator = this.paginator;
-            },
-            () => {},
-            () => {
-                this.ngxLoader.stopLoader(this.chartName);
-            }
-        );
+        this.fuelreqsService
+            .getCustomerCaptureRate(
+                this.groupId,
+                this.fboId,
+                startDate,
+                endDate
+            )
+            .subscribe(
+                (data: CustomerCaptureRateReport[]) => {
+                    this.dataSource.data = data;
+                    this.dataSource.sort = this.sort;
+                    this.paginator.pageIndex = 0;
+                    this.dataSource.paginator = this.paginator;
+                },
+                () => {},
+                () => {
+                    this.ngxLoader.stopLoader(this.chartName);
+                }
+            );
+    }
+    // Custom sorting function
+    private getPropertyValue(item: any, property: string): any {
+        const properties = property.split('.');
+        return properties.reduce((obj, prop) => obj && obj[prop], item);
     }
     exportCsv() {
-        let computePropertyFnc = (item: any[], id: string): any => {
-            if(id == "percentCustomerBusiness"){
-                let color = "00FF00"; // green
-                if(item[id] == 0)
-                    color = "FF0000"; // red
-                return { v: item[id], s: { fill: { fgColor: { rgb: color } } } };
-            }else{
-                return { v: item[id] };
+        const data = this.dataSource.data.slice();
+
+        if (!this.sort.active || this.sort.direction === '') {
+            this.dataSource.data = data;
+            return;
+        }
+
+        // Custom sorting logic for three columns
+        this.dataSource.data = data.sort((a, b) => {
+            const firstLevel = 'percentCustomerBusiness';
+            const valueA = this.getPropertyValue(a, this.sort.active);
+            const valueB = this.getPropertyValue(b, this.sort.active);
+
+            if (valueA < valueB) {
+                return this.sort.direction === 'asc' ? -1 : 1;
+            } else if (valueA > valueB) {
+                return this.sort.direction === 'asc' ? 1 : -1;
             }
 
-        }
-        this.exportCsvFile(this.columns,this.csvFileOptions.fileName,this.csvFileOptions.sheetName,computePropertyFnc);
+            // If values at the first level are equal, go to the next level
+            const secondLevel = 'totalOrders';
+            const secondLevelA = this.getPropertyValue(a, secondLevel);
+            const secondLevelB = this.getPropertyValue(b, secondLevel);
+
+            if (secondLevelA < secondLevelB) {
+                return this.sort.direction === 'asc' ? -1 : 1;
+            } else if (secondLevelA > secondLevelB) {
+                return this.sort.direction === 'asc' ? 1 : -1;
+            }
+
+            // If values at the second level are equal, go to the next level
+            const thirdLevel = 'airportOrders';
+            const thirdLevelA = this.getPropertyValue(a, thirdLevel);
+            const thirdLevelB = this.getPropertyValue(b, thirdLevel);
+
+            if (thirdLevelA < thirdLevelB) {
+                return this.sort.direction === 'asc' ? -1 : 1;
+            } else if (thirdLevelA > thirdLevelB) {
+                return this.sort.direction === 'asc' ? 1 : -1;
+            }
+
+            const fouthLevel = 'company';
+            const fouthLevelA = this.getPropertyValue(a, fouthLevel);
+            const fouthLevelB = this.getPropertyValue(b, fouthLevel);
+
+            if (thirdLevelA < thirdLevelB) {
+                return this.sort.direction === 'asc' ? -1 : 1;
+            } else if (thirdLevelA > thirdLevelB) {
+                return this.sort.direction === 'asc' ? 1 : -1;
+            }
+
+            // If values at the third level are equal, return 0
+            return 0;
+        });
+        let computePropertyFnc = (item: any[], id: string): any => {
+            if (id == 'percentCustomerBusiness') {
+                let color = '00FF00'; // green
+                if (item[id] == 0) color = 'FF0000'; // red
+                return {
+                    v: item[id],
+                    s: { fill: { fgColor: { rgb: color } } },
+                };
+            } else {
+                return { v: item[id] };
+            }
+        };
+
+        this.exportCsvFile(
+            this.columns,
+            this.csvFileOptions.fileName,
+            this.csvFileOptions.sheetName,
+            computePropertyFnc
+        );
     }
 
     applyPresetDateFilter(filter: SelectedDateFilter) {
@@ -124,11 +212,15 @@ export class CustomerCaptureRateComponent extends GridBase implements OnInit {
     }
     openSettings() {
         var _this = this;
-        this.openSettingsDialog(this.tableSettingsDialog, this.columns, function (result) {
-            _this.columns = result;
-            _this.refreshSort(_this.sort, _this.columns);
-            _this.saveSettings(this.tableLocalStorageKey,this.columns);
-        });
+        this.openSettingsDialog(
+            this.tableSettingsDialog,
+            this.columns,
+            function (result) {
+                _this.columns = result;
+                _this.refreshSort(_this.sort, _this.columns);
+                _this.saveSettings(this.tableLocalStorageKey, this.columns);
+            }
+        );
     }
     get visibleColumns() {
         return (
