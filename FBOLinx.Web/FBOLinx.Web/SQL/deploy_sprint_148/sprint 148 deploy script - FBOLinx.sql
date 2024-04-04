a134f933132ba1d26f1ba45a7db74f185fd084bd
robert-the-lawful-head <robertlawhead1@gmail.com>
2024-04-04 10:43:51 -0700

    revised deploy script

--- FBOLinx.Web/FBOLinx.Web/SQL/deploy_sprint_148/sprint 148 deploy script - FBOLinx.sql
+++ FBOLinx.Web/FBOLinx.Web/SQL/deploy_sprint_148/sprint 148 deploy script - FBOLinx.sql
@@ -82,16 +82,6 @@ GO
 USE [paragon_test]
 GO
 
-CREATE UNIQUE NONCLUSTERED INDEX [INX_OrderDetails_FuelerLinxTransactionIdFboHandlerIdAssociatedFuelOrderId] ON [dbo].[OrderDetails]
-(
-	[FuelerLinxTransactionId] ASC,
-	[FboHandlerId] ASC,
-	AssociatedFuelOrderId ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-GO
-USE [paragon_test]
-GO
-
 /****** Object:  Table [dbo].[OrderDetails]    Script Date: 6/26/2023 5:34:40 PM ******/
 SET ANSI_NULLS ON
 GO
@@ -124,6 +114,17 @@ IsOkToEmail bit null
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
+USE [paragon_test]
+GO
+
+CREATE UNIQUE NONCLUSTERED INDEX [INX_OrderDetails_FuelerLinxTransactionIdFboHandlerIdAssociatedFuelOrderId] ON [dbo].[OrderDetails]
+(
+	[FuelerLinxTransactionId] ASC,
+	[FboHandlerId] ASC,
+	AssociatedFuelOrderId ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+GO
+
 USE [paragon_test]
 GO
 
