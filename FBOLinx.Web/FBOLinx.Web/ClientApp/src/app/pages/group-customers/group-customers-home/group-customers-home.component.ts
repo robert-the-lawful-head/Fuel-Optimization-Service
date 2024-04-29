fcc478db7b44dc0b7a99adaf6274fded92eb03ad
robert-the-lawful-head <robertlawhead1@gmail.com>
2024-04-29 13:15:16 -0600

    change spinner to ngx loader on customer group

--- FBOLinx.Web/FBOLinx.Web/ClientApp/src/app/pages/group-customers/group-customers-home/group-customers-home.component.ts
+++ FBOLinx.Web/FBOLinx.Web/ClientApp/src/app/pages/group-customers/group-customers-home/group-customers-home.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { SharedService } from '../../../layouts/shared-service';
 // Services
 import { CustomerinfobygroupService } from '../../../services/customerinfobygroup.service';
+import { NgxUiLoaderService } from 'ngx-ui-loader';
 @Component({
     selector: 'app-group-customers-home',
     styleUrls: ['./group-customers-home.component.scss'],
@@ -12,10 +13,12 @@ export class GroupCustomersHomeComponent implements OnInit {
     // Public Members
     pageTitle = 'Customers';
     customersData: any[];
+    chartName = 'Customers';
 
     constructor(
         private customerInfoByGroupService: CustomerinfobygroupService,
-        private sharedService: SharedService
+        private sharedService: SharedService,
+        private ngxLoader: NgxUiLoaderService
     ) {
         this.sharedService.titleChange(this.pageTitle);
         this.loadCustomers();
@@ -25,11 +28,15 @@ export class GroupCustomersHomeComponent implements OnInit {
 
     // Private Methods
     private loadCustomers() {
+        this.ngxLoader.startLoader(this.chartName);
+
         this.customersData = null;
         this.customerInfoByGroupService
             .getCustomersViewModelByGroup(this.sharedService.currentUser.groupId)
             .subscribe((data: any) => {
                 this.customersData = data;
+                this.ngxLoader.stopLoader(this.chartName);
+
             });
     }
 }
