30487f1ddbc6688e3decea657ae00b186f1aef06
robert-the-lawful-head <robertlawhead1@gmail.com>
2024-08-23 16:59:17 -0600

    improve error messaging by interceptor

--- FBOLinx.Web/FBOLinx.Web/ClientApp/src/app/interceptors/http-error.interceptor.ts
+++ FBOLinx.Web/FBOLinx.Web/ClientApp/src/app/interceptors/http-error.interceptor.ts
@@ -9,32 +9,38 @@ import {
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { SnackBarService } from '../services/utils/snackBar.service';
+import { AuthenticationService } from '../services/authentication.service';
 
 @Injectable()
 export class HttpErrorInterceptor implements HttpInterceptor {
   private errorSnackBarDuration: number = 4000;
-  constructor(private snackbarService: SnackBarService) {}
+  constructor(private snackbarService: SnackBarService,private authenticationService: AuthenticationService) {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(req).pipe(
       catchError((error: HttpErrorResponse) => {
-        console.log("🚀 ~ HttpErrorInterceptor ~ catchError ~ error:", error)
-        let errorMessage = '';
-        let displayError = 'An unexpected error occurred, try reloading the page, if the problem persists contact support';
-        if (error.error instanceof ErrorEvent) {
-          errorMessage = `Error: ${error.error.message}`;
-        } else {
-          errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+
+        if (error.status === 401) {
+          this.authenticationService.logout();
+          location.reload();
+          return;
         }
-        if (error.status === 401 || error.status === 403) {
+
+        let displayError = error.error.message ?? 'An unexpected error occurred, try reloading the page, if the problem persists contact support';
+
+        if(error.error.message != "Username or password is incorrect"){
+          displayError = error.error.message;
+        }
+
+        if (error.status === 403) {
           displayError = 'You are not authorized to access this resource';
         } else if (error.status === 404) {
           displayError = 'The resource you are looking for is not found';
         }
 
-        console.error(errorMessage);
+        console.error(displayError);
         this.snackbarService.showErrorSnackBar(displayError,this.errorSnackBarDuration);
-        return throwError(errorMessage);
+        return throwError(displayError);
       })
     );
   }
