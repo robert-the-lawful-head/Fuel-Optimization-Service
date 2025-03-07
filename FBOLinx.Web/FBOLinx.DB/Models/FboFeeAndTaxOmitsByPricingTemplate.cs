ac2e38c8b4d30022950a6eb8509e960f15f97b63
robert-the-lawful-head <robertlawhead1@gmail.com>
2025-03-07 11:42:05 -0600

    add isomitted flag to persist first change on customer and pricing template omits

--- FBOLinx.Web/FBOLinx.DB/Models/FboFeeAndTaxOmitsByPricingTemplate.cs
+++ FBOLinx.Web/FBOLinx.DB/Models/FboFeeAndTaxOmitsByPricingTemplate.cs
@@ -15,6 +15,7 @@ namespace FBOLinx.DB.Models
         [Column("PricingTemplateID")]
         [Required]
         public int PricingTemplateId { get; set; }
+        public bool? IsOmitted { get; set; }
 
         #region Relationships
         [JsonIgnore]
